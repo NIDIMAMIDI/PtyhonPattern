ebf7334c9d40375a2aa8d95aad53711b74e5fce2
Sharuk <sharuks909@gmail.com>
2023-04-21 11:40:49 +0530

    pyramidPattern

---
+++ pattern3.py
@@ -0,0 +1,7 @@
+n = int(input("Enter number of rows : "))
+for i in range(n):
+    for j in range(n-i-1):
+        print(" ", end="")
+    for j in range(2*i+1):
+        print("*", end ="")
+    print()
